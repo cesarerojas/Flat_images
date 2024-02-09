9c1afbaa6132e2bf0396367acac5a0b015f7244b
cesarerojas <75632661+cesarerojas@users.noreply.github.com>
2024-02-09 13:51:08 -0600

    Update Disputed_ImgsFlat__1and15imgsFgP_Detail.m

--- Disputed_ImgsFlat__1and15imgsFgP_Detail.m
+++ Disputed_ImgsFlat__1and15imgsFgP_Detail.m
@@ -26,7 +26,7 @@ function [] = Disputed_ImgsFlat__1and15imgsFgP_Detail(RESULTS_folder, numCaptDev
 %      Leonardo Palacios-Luengas,     UAM-Iztapalapa.   
 %      Rubén Vázquez-Medina,          IPN-CICATA Querétaro.
 
-% Contact: rvazquez@ipn.mx | February 2024
+% Contact: ruvazquez@ipn.mx | February 2024
 %
 % -------------------------------------------------------------------------
 % DESCRIPTION:
